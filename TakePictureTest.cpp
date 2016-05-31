ccc5854b3b9aabd92f2390fc85e2a87c6cfb8f4f
noeliecherrier <noelie.cherrier@mines-paristech.fr>
2016-05-31 17:20:24 +0200

    Version qui marche

--- TakePictureTest.cpp
+++ TakePictureTest.cpp
@@ -12,16 +12,6 @@ using namespace std;
 
 // Slot
 void TakePictureTest::takePicture() {
-	QImage imagetest;
-	imagetest = sync->GetImage();
-	//QImage* imagerecup = sync->GetImageRecup();
-	/*if (res) {
-		label->setPixmap(QPixmap::fromImage(imagetest));
-	}*/
-
-	/*QString fichier = QFileDialog::getOpenFileName(this, QString(), QString(), "*.jpg *.gif *.png");
-	//QMessageBox::information(this, "Fichier", "Vous avez sélectionné :\n" + fichier);
-	label->setPixmap(fichier);*/
 }
 
 // Default Constructor
@@ -42,20 +32,21 @@ goButton = new QPushButton("Take picture");
 
 // Layout
 SynchronousGrab sync = SynchronousGrab::SynchronousGrab();
-
+QImage imagetest = sync.GetImage();
 label = new QLabel;
-//label->resize(imagetest.size());
 label->setFixedWidth(200);
 label->setFixedHeight(200);
-//timer = new QTimer(this);
-//timer->setInterval(1000); // toutes les 100ms (10 fois par seconde)
-//QImage *image2 = &imagetest;
-QObject::connect(goButton, SIGNAL(clicked()), this, SLOT(takePicture()));
-/*QObject::connect(timer, SIGNAL(timeout()), label, SLOT(setPixmap(QPixmap::fromImage(image2))));
-QObject::connect(goButton, SIGNAL(clicked()), &sync, SLOT(GetImage(image2)));
-QObject::connect(goButton, SIGNAL(clicked()), label, SLOT(setPixmap(QPixmap::fromImage(image2))));
+label->setPixmap(QPixmap::fromImage(imagetest));
+timer = new QTimer(this);
+timer->setInterval(1000); // toutes les 100ms (10 fois par seconde)
+//QObject::connect(timer, SIGNAL(timeout()), label, SLOT(setPixmap(QPixmap::fromImage(sync.GetImage()))));
+QObject::connect(goButton, SIGNAL(clicked()), label, SLOT(setText("Essai")));
 timer->start();
-*/
+label->setVisible(true);
+
+
+
+
 
 
 QGridLayout *layout = new QGridLayout;
